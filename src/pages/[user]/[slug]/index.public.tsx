8032d2b91e427df05e42091610d824c661054d2d
Elias Souza <eliasnsouza1245@gmail.com>
2023-02-13 18:41:18 -0300

    .

--- src/pages/[user]/[slug]/index.public.tsx
+++ src/pages/[user]/[slug]/index.public.tsx
@@ -1,4 +1,6 @@
-import { AuthorTag, DefaultLayout, Viewer } from "@/pages/interface";
+import { Box, Divider, Flex, Heading, Stack, Text, Icon } from "@chakra-ui/react";
+import { AuthorTag, DefaultLayout, LinkComponent, Viewer } from "@/pages/interface";
+import { FaRegCommentAlt } from "react-icons/fa"
 import { ParsedUrlQuery } from "querystring";
 import { GetStaticProps } from "next/types"
 import { GetStaticPaths } from "next"
@@ -6,26 +8,45 @@ import { PostData } from "@/types";
 import { WithId } from "mongodb";
 
 import api from "@/services/api";
-  import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/react";
 
 interface IProps {
   post: WithId<PostData>
+  children: WithId<PostData>[]
+  parent?: WithId<PostData>
 }
 
-export default function Slug({ post }: IProps) {
+export default function Post({ post, children, parent }: IProps) {
+
 
   return (
     <DefaultLayout title={post.title || post.body}>
       <Stack direction="row" spacing={6} px={2}>
         <Box borderRight="1px dotted #62356955" w="1px"></Box>
         <Box>
+          {
+            !post.title &&
+            <Stack direction="row" mb={4} align="center">
+              <Icon as={FaRegCommentAlt}/>
+              <Text noOfLines={1} fontSize="sm">
+                Em resposta a 
+                <LinkComponent 
+                  fontWeight={600} 
+                  href={`/${parent?.author}/${parent?.slug}`}
+                > {parent?.title || parent?.body}
+                </LinkComponent>
+              </Text>
+            </Stack>
+          }
           <AuthorTag
             post={post}
           />
-          <Heading my={2} fontSize="2em" fontWeight={500}>
-            {post.title}
-          </Heading>
-          <Box bg="Red">
+          {
+            post.title &&
+            <Heading my={2} fontSize="2em" fontWeight={500}>
+              {post.title}
+            </Heading>
+          }
+          <Box mt={2} bg="Red">
             <Viewer
               value={post.body}
             />
@@ -40,7 +61,7 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 
   const postsResponse = await api.get<WithId<PostData>[]>("/contents")
   const posts = postsResponse.data
-
+  
   const paths = posts.map(post => ({ params: { slug: post.slug, user: post.author }}))
 
   return {
@@ -58,15 +79,40 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const { user, slug } = context.params as IParams  
   
+  try {
+    const parentData = await api.get<WithId<PostData>>(`/contents/${user}/${slug}/parent`)
+    const parent = parentData.data
+
+    const childrenData = await api.get<WithId<PostData>[]>(`/contents/${user}/${slug}/children`)
+    const children = childrenData.data
+
+    const response = await api.get<WithId<PostData>>(`/contents/${user}/${slug}`)
+    const post = response.data
+    
+    return {
+      props: {
+        post,
+        children,
+        parent
+      },
+      revalidate: 60
+    }
+    
+  } catch (error) {
+    console.log(error);
+  }
   
   try {
-    
+    const childrenData = await api.get<WithId<PostData>[]>(`/contents/${user}/${slug}/children`)
+    const children = childrenData.data
+
     const response = await api.get<WithId<PostData>>(`/contents/${user}/${slug}`)
     const post = response.data
 
     return {
       props: {
-        post
+        post,
+        children,
       },
       revalidate: 60
     }
--- src/pages/api/contents/[user]/[slug]/parent/index.public.ts
+++ src/pages/api/contents/[user]/[slug]/parent/index.public.ts
@@ -1,6 +1,6 @@
 import { NotFoundError } from "@/errors";
 import { connectToDatabase } from "@/services/database";
-import { PostData } from "@/types";
+import { PostData, UserData } from "@/types";
 import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -10,7 +10,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const db = await connectToDatabase()
   const postCol = db.collection<PostData>("posts")
-  const userFinded = await db.collection("users").findOne({ username: user})
+  const userFinded = await db.collection<UserData>("users").findOne({ username: user })
   const thisPost = await postCol.findOne({ slug: slug, author: user })
 
   if (!userFinded) return res.status(404).send(new NotFoundError(
