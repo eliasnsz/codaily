2f8fed5662ce68d0d8173780b9ecd339f9b6394d
Elias Souza <eliasnsouza1245@gmail.com>
2023-02-14 16:38:27 -0300

    .

--- src/pages/api/contents/[user]/[slug]/index.public.ts
+++ src/pages/api/contents/[user]/[slug]/index.public.ts
@@ -79,6 +79,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     await Promise.all([
       res.revalidate("/"),
       res.revalidate(`/${author}/${thisPost?.slug}`),
+      res.revalidate(`/${author}/${newComment?.slug}`),
       res.revalidate(`/${author}`)
     ])
 
